03bd0ffc0a81a83a17823ed345723a7214891608
Erwin Gribnau <erwin@gribnau.org>
2018-01-12 08:58:24 +0100

    Reduce the amount of dependencies required

--- setting_up/install_ubuntu_packages.sh
+++ setting_up/install_ubuntu_packages.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# This script installs the minimum dependencies to get the rustup,
+# cargo, xargo and bobbin installed and running
+
 apt update
-apt install -y curl bzip2 build-essential dfu-util git file
+apt install -y curl bzip2 dfu-util git gcc make
+
+# you could install the 'build-essential' package if you expect
+# to do more development that uses native C libraries
