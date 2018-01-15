4e8286e6203a0a1afd32b9942b94f13197917385
Erwin Gribnau <erwin.gribnau@technolution.nl>
2018-01-15 10:54:55 +0100

    Improved checking for existing cargo installed commands

--- setting_up/setup.sh
+++ setting_up/setup.sh
@@ -2,15 +2,10 @@
 
 clear
 
-# TODO: test for the existence of:
-# curl
-# tar
-# bzip2
-# cc & ld (maybe)
-# needed apt install curl bzip2 build-essential
-
-GREEN='\033[0;92m'
-MAG='\033[0;35m'
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+MAG='\033[0;1;35m'
+BOLD='\033[0;1m'
 RESET='\033[0m'
 
 function highlight {
@@ -23,30 +18,49 @@ function highlight {
     printf "$RESET\n"
 }
 
-env_file=~/.cargo/env
+function cargo_install_helper {
+    CMD="command -v $1"
+    EXISTING_INSTALL=$($CMD)
+    if [ "" != "$EXISTING_INSTALL" ]
+    then
+        highlight "Found existing" "$1" "at $EXISTING_INSTALL"
+        highlight "\tif neccessary upgrade with 'cargo install --force $2'"
+    else
+        highlight "Installing (= building)" "$2" ", $3"
+        cargo install $2
+    fi
+}
 
-if [ -e "$env_file" ]
+# STEP 1: Rustup toolchain manager, rust compiler and cargo build tool
+CARGO_ENV_FILE=~/.cargo/env
+if [ -e "$CARGO_ENV_FILE" ]
 then
 	highlight "Existing" "rustup" "found, running an update"
-	source $env_file
+	source $CARGO_ENV_FILE
 	rustup update
     highlight "Installing the" "Rust nightly toolchain"
     rustup toolchain add nightly
 else
 	highlight "Installing"  "rustup" "with the nightly toolchain"
 	curl https://sh.rustup.rs -sSf | sh -s -- -y --default-toolchain nightly
-	source $env_file
+	source $CARGO_ENV_FILE
 fi
 
+# STEP 2: Defaulting to the nightly compiler
 highlight "Selecting the" "rust nightly toolchain" "as default"
 rustup default nightly
 
+# STEP 3: Adding the Rust sources (needed by Xargo)
 highlight "Installing" "rust stdlib sources"
 rustup component add rust-src
 
-if [ -e "$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin/arm-none-eabi-ld" ]
+# STEP 4: Installing the GNU Arm Embedded Toolchain
+export PATH=$PATH:"$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin/"
+EXISTING_ARM_LOC=`command -v arm-none-eabi-ld`
+
+if [ -x $EXISTING_ARM_LOC ]
 then
-    highlight "Found" "GNU Arm Embedded Toolchain"
+    highlight "Found" "GNU Arm Embedded Toolchain" "at $EXISTING_ARM_LOC"
 else
     if [ -e /tmp/gcc-arm.tar.bz2 ]
     then
@@ -59,17 +73,16 @@ else
     mkdir -p ~/.armtoolchain
     highlight "Extracting" "GNU Arm Embedded toolchain"
     cd ~/.armtoolchain && tar jxf /tmp/gcc-arm.tar.bz2
+    highlight "Please add" "$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin/" "to your PATH"
 fi
 
-export PATH=$PATH:"$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin/"
-highlight "Please add" "$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin/" "to your PATH"
+# STEP 5: Installing Xargo
+cargo_install_helper xargo xargo "the cross compilation toolchain for Rust"
 
-highlight "Installing (= building)" "xargo" ", the cross compilation toolchain for Rust"
-cargo install xargo
+# STEP 6: Installing bobbin-cli
+cargo_install_helper bobbin bobbin-cli "the embedded development tool for Rust"
 
-highlight "Installing (=building)" "bobbin-cli" ", the embedded development tool for Rust"
-cargo install --git https://github.com/bobbin-rs/bobbin-cli.git
-
+# STEP 7: Checking and informing about PATH
 highlight "Running" "bobbin check" "to check the installation"
 bobbin check
 
@@ -82,9 +95,15 @@ highlight "GCC"         "\t> 7.2.1 \t" "check fails currently, check by hand usi
 highlight "dfu-util"    "\t> 0.8 \t\t" "(install with your package manager if not present and re-run 'bobbin check')"
 
 printf "\n"
-highlight "Script is done, now you need to do some work!\n"
-highlight "Add rustup and cargo to your PATH, so run in you shell:" "\tsource ~/.cargo/env"
-highlight "Add ARM toolchain to you PATH, so run in you shell:" "\texport PATH=\$PATH:$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin"
+highlight "" "Script is done, now YOU need to do some work!\n"
+highlight "Add rustup and cargo to your PATH,"
+highlight "" "so run in you shell:" "\tsource ~/.cargo/env"
+highlight "Add ARM toolchain to you PATH if it was installed by this script, so run in you shell:"
+highlight "" "\texport PATH=\$PATH:$HOME/.armtoolchain/gcc-arm-none-eabi-7-2017-q4-major/bin"
 
 printf "\n"
-highlight "And yes, it is a smart move to put this stuff in your" ".profile" "(rustup does that by default, so all tools except the ARM toolchain should work after a logout/login)"
+highlight "And yes, it is a smart move to put this stuff in your" ".profile"
+highlight "(rustup does that by default, so all tools except the ARM toolchain"
+highlight "should work after a logout/login)"
+
+
